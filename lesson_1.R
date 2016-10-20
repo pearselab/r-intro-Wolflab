5c0b6313ae77c8700bef3fd58335a106c54d694c
Paul Wolf <Paul.wolf@usu.edu>
2016-10-19 20:27:46 -0600

    a few more from the unhelp menu

--- lesson_1.R
+++ lesson_1.R
@@ -12,7 +12,7 @@ c(1,2, "three")
 
 #OK - here we go
 # cat in R
-# Well, I thought I understood this until I read the help file. I had already read that c means
+# Well, I thought I understood this until I read the help file. I had already read what c means
 # concatenate. Now I find that cat aloso means concatenate. 
 # would be nice if different functions had different names
 # cat seems to join things and send the joined stuff to screen.  Let's try:
@@ -20,7 +20,26 @@ cat("hello", " world")
 # now:
 x = c("hello", " world")
 x
-#ok - this is starting to make a little sense
+#ok - this is starting to make a little sense. So c is concatenate, where as 
+# cat is a print function.
+# UP next: cbind
+a <- c(1,2,3,4,5,6)
+b <- c("a", "b", "c", "d", "e", "f")
+c <- c(1.1, 2.1, 3.1, 4.4, 5.5, 6.6)
+cbind(a,b,c)
+rbind(a,b,c)
+# I can do this. But the examples in the help menue make no sense becasue they are trying to cbind
+# things that don't exists yet. I'll juust keep it simple for now.
+m1 <- matrix(1:9, nrow=3, ncol = 3)
+m2 <- matrix(34:42,nrow=3, ncol = 3)
+m3 <- matrix(54:70, ncol=3, nrow=4)
+cbind(m1,m2)
+rbind(m1,m2)
+cbind(m1,m3)# this should fail
+rbind(m1,m3)
+# Must have same number of rows to cbind a matrices
+# Must have same number of cols to rbind matrices
+
 
 ################################################
 ## Bonus exercises #############################
