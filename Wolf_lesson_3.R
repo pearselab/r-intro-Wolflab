fabef62953a60a96e574688ae7daecfc2f98cc22
Paul Wolf <Paul.wolf@usu.edu>
2016-10-26 10:06:14 -0600

    attempting to start Q13

--- Wolf_lesson_3.R
+++ Wolf_lesson_3.R
@@ -1,1 +1,7 @@
-print(dbinom(5, 10, 0.5))
+pussy <- list(length=20, weight=10, breed="Bristol")
+class(pussy) <- "feline" # I don't want to use "cat" because I'll get confused with the method "cat"
+
+print.feline <- function(x, ...){
+  cat("This ", x$breed, "weighs ", x$weight, "kg\n")
+}
+
